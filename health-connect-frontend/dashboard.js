292d10db4f51a4c47cd79edb05b6ee6342200154
ZannatulElma <zannatul2@iut-dhaka.edu>
2023-09-07 18:02:46 +0600

    dashboard responsive(1200px) created

--- health-connect-frontend/dashboard.js
+++ health-connect-frontend/dashboard.js
@@ -1,8 +1,17 @@
 const hamburger = document.querySelector(".hamburger-menu");
-const hamburger_options = document.querySelector(".options");
+// const hamburger_options = document.querySelector(".options");
+const aside = document.querySelector('aside');
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
-    hamburger_options.classList.toggle("active");
+    aside.classList.toggle("aside-active");
+    // if (hamburger.classList.contains('active')) {
+    //     aside.style.display = "block";
+    //     aside.style.position = "fixed";
+    // }
+    // else {
+    //     aside.style.display = "none";
+    //     aside.style.position = "";
+    // }
 
 })
