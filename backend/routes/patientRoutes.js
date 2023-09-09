098121968f23c579c4ec16574b1c614209031ab9
cabbage <shakun650@gmail.com>
2023-09-09 10:27:15 +0600

    Route and controller setup for patient appointment

---
+++ backend/routes/patientRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const requireAuth = require("../middlewares/requireAuth");
+
+const {} = require("../controllers/patientControllers");
+
+const router = express.Router();
+
+module.exports = router;
