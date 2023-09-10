84c73047836cc647974fff97a5a16e72caa67fbb
cabbage <shakun650@gmail.com>
2023-09-10 14:47:15 +0600

    Added baseUrls for apis with commented api calls

---
+++ frontend/health-connect/src/apis/AdminApi.jsx
@@ -0,0 +1,76 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://localhost:5000/api/admin",
+});
+
+/* To use the api, import this file as AdminApi */
+//just copy/paste the following functions inside try catch in the hooks
+// will get the data in response.data if no error
+//also must use useAuthContext hook in every page to check sessions
+//start/end date time must be in format    YYYY-MM-DDT21:00:00   , then use new date(YYYY-MM-DDT21:00:00) before giving it in body
+
+/** Adding appointment info (town optional)*/
+
+// const response = await AdminApi.post(
+//     "/add-one-appointment",
+//     {
+    // doctorName,
+    // doctorId,
+    // startTime,
+    // endTime,
+    // hospitalName,
+    // hospitalId,
+    // district,
+    // town,
+//     },
+//     {
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     }
+//   );
+
+/** Adding hospital (town optional)*/
+
+// const response = await AdminApi.post(
+//     "/add-hospital",
+//     {
+    // hospitalName,
+    // district,
+    // town,
+    // totalBeds,
+    // totalCabins,
+    // availableBeds,
+    // availableCabins,
+//     },
+//     {
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     }
+//   );
+
+
+/** Adding one doctor*/
+
+// const response = await AdminApi.post(
+//     "/add-one-doctor",
+//     {
+    // fullname,
+    // hospitalName,
+    // hospitalId,
+    // dob,
+    // education,
+    // gender,
+    // contact,
+    // email
+//     },
+//     {
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     }
+//   );
+
+
---
+++ frontend/health-connect/src/apis/PatientApi.jsx
@@ -0,0 +1,97 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://localhost:5000/api/patient",
+});
+
+/* To use the api, import this file as PatientApi */
+//just copy/paste the following functions inside try catch in the hooks
+// will get the data in response.data if no error
+//also must use useAuthContext hook in every page to check sessions
+//start/end date time must be in format    YYYY-MM-DDT21:00:00   , then use new date(YYYY-MM-DDT21:00:00) before giving it in body
+
+/** Updating patient information (height and weight optional)*/
+
+// const response = await PatientApi.put(
+//     "/update-patient",
+//     {
+//       email, fullname, address, dob, height, weight,gender, contact
+//     },
+//     {
+//       headers: {
+//         Authorization: `Bearer ${user.token}`,
+//         "Content-Type": "application/json",
+//       },
+//     }
+//   );
+
+
+/** Patient adds appointment*/
+
+// const response = await PatientApi.post(
+//     "/add-appointment",
+//     {
+          // doctorName,
+          // doctorId,
+          // patientName,
+          // startTime,
+          // endTime,
+          // hospitalName,
+          // hospitalId,
+          // district,
+          // town,
+//     },
+//     {
+//       headers: {
+//         Authorization: `Bearer ${user.token}`,
+//         "Content-Type": "application/json",
+//       },
+//     }
+//   );
+
+
+/** Patient gets previous appointment*/
+
+// const response = await PatientApi.get(
+//     "/add-appointment",
+//     {
+//       headers: {
+//         Authorization: `Bearer ${user.token}`,
+//       },
+//     }
+//   );
+
+
+/** Patient gets upcoming appointments*/
+
+// const response = await PatientApi.get(
+//     "/upcoming-appointment",
+//     {
+//       headers: {
+//         Authorization: `Bearer ${user.token}`,
+//       },
+//     }
+//   );
+
+
+/** Patient gets available appointments*/
+
+// const response = await PatientApi.get(
+//     "/get-all-appointment",
+//     {
+//       headers: {
+//         Authorization: `Bearer ${user.token}`,
+//       },
+//     }
+//   );
+
+/** Patient gets hospital list*/
+
+// const response = await PatientApi.get(
+//     "/get-all-hospital",
+//     {
+//       headers: {
+//         Authorization: `Bearer ${user.token}`,
+//       },
+//     }
+//   );
