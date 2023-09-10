0e178867dcf171d6c83e3b1cd7f335fc8ed21ab6
thdihan <thdihan@outlook.com>
2023-09-10 11:57:14 +0600

    appointment and doctor card css fixed

--- frontend/health-connect/src/components/dashboard/SingleAppointment.jsx
+++ frontend/health-connect/src/components/dashboard/SingleAppointment.jsx
@@ -5,10 +5,10 @@ export default function SingleAppointment({ className }) {
             <div className={classes["single-appoint-card_1strow"]}>
                 <h3>Doctor name:abc</h3>
                 <h3>Specialization:abc</h3>
-                <h3>serial:03</h3>
             </div>
             <div className={classes["single-appoint-card_2ndrow"]}>
                 <h3>location:abc</h3>
+                <h3>serial:03</h3>
                 <h3>date:08/09/2023</h3>
                 <h3>status:done</h3>
             </div>
--- frontend/health-connect/src/components/dashboard/SingleDoctor.jsx
+++ frontend/health-connect/src/components/dashboard/SingleDoctor.jsx
@@ -5,17 +5,17 @@ export default function SingleDoctor() {
             <div className={classes["single-bookappoint-card_1strow"]}>
                 <h3>Doctor name:abc</h3>
                 <h3>Specialization:abc</h3>
-
-                <h3>Patient-Record:03</h3>
             </div>
             <div className={classes["single-bookappoint-card_2ndrow"]}>
                 <h3>location:abc</h3>
                 <h3>date:08/09/2023</h3>
-                <button className={classes["appoint-book-btn"]}>
-                    <a href="./takeappointment.html">
-                        <h3>Book Appointment</h3>
-                    </a>
-                </button>
+                <h3>Patient-Record:03</h3>
+                <a
+                    className={classes["appoint-book-btn"]}
+                    href="./takeappointment.html"
+                >
+                    Book Appointment
+                </a>
             </div>
         </div>
     );
