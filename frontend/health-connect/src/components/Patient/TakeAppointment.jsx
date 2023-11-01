f434f97e635fe9b6b008aa6a13f128c2f1ef1d38
thdihan <thdihan@outlook.com>
2023-11-02 00:10:07 +0600

    Filter Option added

--- frontend/health-connect/src/components/Patient/TakeAppointment.jsx
+++ frontend/health-connect/src/components/Patient/TakeAppointment.jsx
@@ -4,19 +4,105 @@ import { useAuthContext } from "../../hooks/useAuthContext";
 import { useDoctorList } from "../../hooks/useDoctorList";
 import classes from "../../styles/TakeAppointment.module.css";
 import SingleDoctor from "./singleDoctor";
+import _ from "lodash";
 
 export default function TakeAppointment() {
+    const [specializationList, setSpecializationList] = useState([]);
+    const [distrcitList, setDistrictList] = useState([]);
     const { user } = useAuthContext();
-    console.log("user : ", user);
 
-    const { doctorData, doctorLoading, doctorError } = useDoctorList(user);
-    console.log("TAKE APPOINTMENT PAGE : DOCTOR DATA", doctorData);
+    const {
+        doctorData,
+        doctorLoading,
+        doctorError,
+        specializationFilter,
+        districtFilter,
+        sortByDistance,
+        setSpecializationFilter,
+        setDistrictFilter,
+        setSortByDistance,
+    } = useDoctorList(user);
     const { doctorList } = doctorData;
 
-    console.log("Take Appointment Data  from hook: ", doctorData);
+    useEffect(() => {
+        const allSpecialization = [];
+        const allDistrict = [];
+
+        doctorList?.forEach((singleDoctor) => {
+            singleDoctor.specializations.forEach((specialization) => {
+                if (!allSpecialization.includes(specialization)) {
+                    // Avoid duplicates
+                    allSpecialization.push(specialization);
+                }
+            });
+            if (!allDistrict.includes(singleDoctor.address.district)) {
+                // Avoid duplicates
+                allDistrict.push(singleDoctor.address.district);
+            }
+        });
+
+        setSpecializationList(allSpecialization);
+        setDistrictList(allDistrict);
+    }, [doctorList]);
 
     return (
         <section className={classes["take-appointment-part"]}>
+            <div className={classes["filter-field"]}>
+                <form>
+                    <label htmlFor="filter-specialization">
+                        Specializations :{" "}
+                    </label>
+                    <select
+                        name="filter-specialization"
+                        id="filter-specialization"
+                        value={specializationFilter}
+                        onChange={(e) => {
+                            console.log(
+                                "Specialization Value :",
+                                e.target.value
+                            );
+                            setSpecializationFilter(e.target.value);
+                        }}
+                    >
+                        {specializationList?.map((specialization) => (
+                            <option
+                                key={specialization}
+                                value={_.toLower(specialization)}
+                            >
+                                {specialization}
+                            </option>
+                        ))}
+                    </select>
+
+                    <label htmlFor="filter-district">District : </label>
+                    <select
+                        name="filter-district"
+                        id="filter-district"
+                        value={districtFilter}
+                        onChange={(e) => {
+                            console.log("District Value :", e.target.value);
+                            setDistrictFilter(e.target.value);
+                        }}
+                    >
+                        {distrcitList?.map((district) => (
+                            <option key={district} value={_.toLower(district)}>
+                                {district}
+                            </option>
+                        ))}
+                    </select>
+                    <label htmlFor="filter-distance">Sort By Distance</label>
+                    <input
+                        type="checkbox"
+                        name="filter-distance"
+                        id="filter-distance"
+                        checked={sortByDistance}
+                        onChange={() => {
+                            setSortByDistance((prev) => (prev ? false : true));
+                        }}
+                    />
+                </form>
+            </div>
+
             <section className={classes["doc-upcoming-apoint-chk-part"]}>
                 <div className={classes["doc-upcoming-apoint-check-heading"]}>
                     <h2>Available Doctor</h2>
--- frontend/health-connect/src/hooks/useDoctorList.js
+++ frontend/health-connect/src/hooks/useDoctorList.js
@@ -6,89 +6,12 @@ export const useDoctorList = (user) => {
     const [loading, setLoading] = useState(true);
     const [data, setData] = useState([]);
     console.log("user : ", user);
+    const [specializationFilter, setSpecializationFilter] = useState("");
+    const [districtFilter, setDistrictFilter] = useState("");
+    const [sortByDistance, setSortByDistance] = useState(false);
 
-    function bubbleSort(doctorDistance, doctorData) {
-        console.log(doctorData, " dfdfs ", doctorDistance);
-        console.log("SORT FUNCTION CALLED");
-        const n = doctorDistance.length;
-        for (let i = 0; i < n - 1; i++) {
-            for (let j = 0; j < n - i - 1; j++) {
-                const distanceA = parseFloat(doctorDistance[j].distance);
-                const distanceB = parseFloat(doctorDistance[j + 1].distance);
-                if (
-                    isNaN(distanceA) ||
-                    isNaN(distanceB) ||
-                    distanceA > distanceB
-                ) {
-                    // Swap the elements
-                    let temp = doctorDistance[j];
-                    doctorDistance[j] = doctorDistance[j + 1];
-                    doctorDistance[j + 1] = temp;
-
-                    temp = doctorData[j];
-                    doctorData[j] = doctorData[j + 1];
-                    doctorData[j + 1] = temp;
-                }
-            }
-        }
-    }
-
+    // [TODO] :  USE THOSE FILTER STATE
     useEffect(() => {
-        // async function sortByDistance(doctorList) {
-        //     console.log("DOCTOR LIST IN SORT FUNCTION ", doctorList);
-        //     const doctorDistance = [];
-        //     let destination = "destinations=";
-        //     doctorList.forEach((doctor) => {
-        //         doctorDistance.push({
-        //             id: doctor._id,
-        //             lat: doctor.address.latitude,
-        //             lng: doctor.address.longitude,
-        //         });
-        //         destination +=
-        //             doctor.address.latitude +
-        //             "%2C" +
-        //             doctor.address.longitude +
-        //             "%7C";
-        //     });
-        //     destination = destination.slice(0, -3);
-        //     console.log(destination);
-        //     console.log(doctorDistance);
-        //     try {
-        //         /**Get the distance */
-        //         const response = await PatientApi.post(
-        //             "/get-distance",
-        //             {
-        //                 url: `https://maps.googleapis.com/maps/api/distancematrix/json?destinations=${destination}&origins=23.94821289164638%2C90.37926469346104&key=AIzaSyCw9Xz5vT5x6m8DTutXNygenRnDX8jIYXs`,
-        //             },
-        //             {
-        //                 headers: {
-        //                     Authorization: `Bearer ${user.token}`,
-        //                 },
-        //             }
-        //         );
-        //         console.log(response.data.data.rows[0].elements);
-        //         for (
-        //             let i = 0;
-        //             i < response.data.data.rows[0].elements.length;
-        //             i++
-        //         ) {
-        //             console.log(response.data.data.rows[0].elements[i]);
-        //             doctorDistance[i]["distance"] =
-        //                 response.data.data.rows[0].elements[i].status === "OK"
-        //                     ? response.data.data.rows[0].elements[i].distance
-        //                           .text
-        //                     : "9999999 km";
-        //         }
-        //         console.log(doctorDistance);
-        //         console.log("BUBBLE SORT HERE");
-        //         bubbleSort(doctorDistance, doctorList);
-        //         console.log("AFTER SORT ", doctorDistance, doctorList);
-        //         setData(doctorList);
-        //         // setSortedDoctorList(doctorList);
-        //     } catch (err) {
-        //         console.log("SORT FUNCTION ERROR", err);
-        //     }
-        // }
         const fetchAppointmentList = async () => {
             try {
                 /** Patient gets available appointments*/
@@ -120,5 +43,11 @@ export const useDoctorList = (user) => {
         doctorData: data,
         doctorLoading: loading,
         doctorError: error,
+        specializationFilter,
+        districtFilter,
+        sortByDistance,
+        setSpecializationFilter,
+        setDistrictFilter,
+        setSortByDistance,
     };
 };
