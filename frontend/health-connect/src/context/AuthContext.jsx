fb0212cc5b2556e8d5ee533307c86a7d558f1947
thdihan <thdihan@outlook.com>
2023-11-03 00:16:18 +0600

    merge conflict

--- frontend/health-connect/src/context/AuthContext.jsx
+++ frontend/health-connect/src/context/AuthContext.jsx
@@ -4,55 +4,36 @@ import UserLog from "../apis/UserLog";
 export const AuthContext = createContext();
 
 export const AuthContextProvider = (props) => {
-
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
-  const [newUser, setNewUser] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  // const [state, dispatch] = useReducer(authReducer, {
-  //     user: null,
-  // });
-  useEffect(() => {
-    // if (user) {
-    //     dispatch({ type: "LOGIN", payload: user });
-    // }
-    // console.log("IMAS: ", user.token);
-    const getUserDetails = async () => {
-      const response = await UserLog.get("/", {
-        headers: {
-          Authorization: `Bearer ${user.token}`,
-        },
-      });
-      //   console.log("Response from getUserDetails : ", response);
-      setNewUser(response.data.user);
-      // console.log("NewUser: ", newUser);
-    };
-    try {
-      if (user) {
-        getUserDetails();
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }, [user]);
-  // console.log("AuthContext State: ", state);
-
-  const login = async (email, password) => {
-    setLoading(true);
-    try {
-      const response = await UserLog.post(
-        "/login",
-        {
-          email,
-          password,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
+    const [newUser, setNewUser] = useState();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    // const [state, dispatch] = useReducer(authReducer, {
+    //     user: null,
+    // });
+    useEffect(() => {
+        // if (user) {
+        //     dispatch({ type: "LOGIN", payload: user });
+        // }
+        // console.log("IMAS: ", user.token);
+        const getUserDetails = async () => {
+            const response = await UserLog.get("/", {
+                headers: {
+                    Authorization: `Bearer ${user.token}`,
+                },
+            });
+            //   console.log("Response from getUserDetails : ", response);
+            setNewUser(response.data.user);
+            // console.log("NewUser: ", newUser);
+        };
+        try {
+            if (user) {
+                getUserDetails();
+            }
+        } catch (error) {
+            console.log(error);
         }
     }, [user]);
-    // console.log("AuthContext State: ", state);
 
     const login = async (email, password) => {
         setLoading(true);
