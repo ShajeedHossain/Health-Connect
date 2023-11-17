e3dcc30981afc829ad22ad007a587926c752bc85
cabbage <shakun650@gmail.com>
2023-11-17 23:48:03 +0600

    Single appointment bug fixed

--- frontend/health-connect/src/components/Patient/SingleAppointment.jsx
+++ frontend/health-connect/src/components/Patient/SingleAppointment.jsx
@@ -1,149 +1,146 @@
 import { Link } from "react-router-dom";
-import classes from "../../styles/SingleAppointment.module.css";
-import { useEffect, useState } from "react";
 import { formatDateAndTime } from "../../Utility/formateTime";
-import useGetCurrentLatLng from "../../hooks/useGetCurrentLatLng";
 import { useHospitalProfileInfo } from "../../hooks/Hospital/useHospitalProfileInfo";
-import { useAuthContext } from "../../hooks/useAuthContext";
-import HospitalApi from "../../apis/HospitalApi";
+import useGetCurrentLatLng from "../../hooks/useGetCurrentLatLng";
+import classes from "../../styles/SingleAppointment.module.css";
 
 export default function SingleAppointment({
-    className,
-    doctorDetails,
-    previousFlag,
-    user,
+  className,
+  doctorDetails,
+  previousFlag,
+  user,
 }) {
-    const { currentLatitude, currentLongitude } = useGetCurrentLatLng();
-    const {
-        serial,
-        startTime,
-        doctorName,
-        specializations,
-        address,
-        hospitalName,
-        isTaken,
-        hospitalId,
-    } = doctorDetails;
+  const { currentLatitude, currentLongitude } = useGetCurrentLatLng();
+  const {
+    serial,
+    startTime,
+    doctorName,
+    specializations,
+    address,
+    hospitalName,
+    isTaken,
+    hospitalId,
+  } = doctorDetails;
 
-    console.log("FROM SINGLE APPOINTMENT PAGE: ", hospitalId);
+  console.log("FROM SINGLE APPOINTMENT PAGE: ", hospitalId);
 
-    // console.log("HOSPITAL DATA SINGLE APPOINTMENT PAGE: ", data);
+  // console.log("HOSPITAL DATA SINGLE APPOINTMENT PAGE: ", data);
 
-    const { latitude, longitude, district, town } = hospitalId.address;
+  const { latitude, longitude, district, town } = hospitalId.address;
 
-    const { date, time } = formatDateAndTime(startTime);
+  const { date, time } = formatDateAndTime(startTime);
 
-    const { data, loading, error } = useHospitalProfileInfo(hospitalId);
-    console.log("HOSPITAL DATA SINGLE APPOINTMENT PAGE: ", data);
-    // useEffect(() => {
-    //     const fetchHospitalDetails = async () => {
-    //         console.log("SINGLE APPOINTMENT COMPONENT FUNCTION ENTER...");
-    //         try {
-    //             // setLoading(true);
+  const { data, loading, error } = useHospitalProfileInfo(user, hospitalId);
+  console.log("HOSPITAL DATA SINGLE APPOINTMENT PAGE: ", data);
+  // useEffect(() => {
+  //     const fetchHospitalDetails = async () => {
+  //         console.log("SINGLE APPOINTMENT COMPONENT FUNCTION ENTER...");
+  //         try {
+  //             // setLoading(true);
 
-    //             const response = await HospitalApi.get("/get-hospital", {
-    //                 headers: {
-    //                     "Content-Type": "application/json",
-    //                     Authorization: `Bearer ${user.token}`,
-    //                 },
-    //             });
+  //             const response = await HospitalApi.get("/get-hospital", {
+  //                 headers: {
+  //                     "Content-Type": "application/json",
+  //                     Authorization: `Bearer ${user.token}`,
+  //                 },
+  //             });
 
-    //             console.log("GET HOSPITAL HOOK", response.data);
-    //             // setLoading(false);
-    //             // setData(response.data); // Uncomment this line
-    //         } catch (err) {
-    //             console.log(err);
-    //             // setLoading(false);
-    //             // setError(true);
-    //         }
-    //     };
+  //             console.log("GET HOSPITAL HOOK", response.data);
+  //             // setLoading(false);
+  //             // setData(response.data); // Uncomment this line
+  //         } catch (err) {
+  //             console.log(err);
+  //             // setLoading(false);
+  //             // setError(true);
+  //         }
+  //     };
 
-    //     fetchHospitalDetails();
-    // });
+  //     fetchHospitalDetails();
+  // });
 
-    return (
-        <div className={`dashboard-card ${classes.className}`}>
-            <table>
-                <tr>
-                    <td>
-                        <b>Doctor name: </b>
-                    </td>
-                    <td>{doctorName}</td>
-                </tr>
+  return (
+    <div className={`dashboard-card ${classes.className}`}>
+      <table>
+        <tr>
+          <td>
+            <b>Doctor name: </b>
+          </td>
+          <td>{doctorName}</td>
+        </tr>
 
-                <tr>
-                    <td>
-                        <b>Address:</b>
-                    </td>
-                    <td>
-                        {" "}
-                        {hospitalName}, {town}, {district}
-                    </td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td>
-                        <Link
-                            style={{
-                                color: "green",
-                            }}
-                            to={`https://www.google.com/maps/dir/?api=1&origin=${currentLatitude},${currentLongitude}&destination=${latitude},${longitude}`}
-                            target="_blank"
-                        >
-                            <span
-                                style={{
-                                    verticalAlign: "middle",
-                                }}
-                                className="material-symbols-outlined"
-                            >
-                                person_pin_circle
-                            </span>
-                            <span
-                                style={{
-                                    verticalAlign: "middle",
-                                }}
-                            >
-                                Map
-                            </span>
-                        </Link>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <b>Serial:</b>
-                    </td>
-                    <td>{serial}</td>
-                </tr>
-                <tr>
-                    <td>
-                        <b>Date:</b>
-                    </td>
-                    <td>{date}</td>
-                </tr>
-                <tr>
-                    <td>
-                        <b>Time:</b>
-                    </td>
-                    <td> {time}</td>
-                </tr>
+        <tr>
+          <td>
+            <b>Address:</b>
+          </td>
+          <td>
+            {" "}
+            {hospitalName}, {town}, {district}
+          </td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>
+            <Link
+              style={{
+                color: "green",
+              }}
+              to={`https://www.google.com/maps/dir/?api=1&origin=${currentLatitude},${currentLongitude}&destination=${latitude},${longitude}`}
+              target="_blank"
+            >
+              <span
+                style={{
+                  verticalAlign: "middle",
+                }}
+                className="material-symbols-outlined"
+              >
+                person_pin_circle
+              </span>
+              <span
+                style={{
+                  verticalAlign: "middle",
+                }}
+              >
+                Map
+              </span>
+            </Link>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <b>Serial:</b>
+          </td>
+          <td>{serial}</td>
+        </tr>
+        <tr>
+          <td>
+            <b>Date:</b>
+          </td>
+          <td>{date}</td>
+        </tr>
+        <tr>
+          <td>
+            <b>Time:</b>
+          </td>
+          <td> {time}</td>
+        </tr>
 
-                {isTaken && (
-                    <tr>
-                        <td></td>
-                        <td>
-                            <Link
-                                state={{
-                                    appointmentDetails: doctorDetails,
-                                }}
-                                className={"appoint-book-btn"}
-                                to="/doctor-dashboard/previous-history"
-                            >
-                                View Details
-                            </Link>
-                        </td>
-                    </tr>
-                )}
-            </table>
-        </div>
-    );
+        {isTaken && (
+          <tr>
+            <td></td>
+            <td>
+              <Link
+                state={{
+                  appointmentDetails: doctorDetails,
+                }}
+                className={"appoint-book-btn"}
+                to="/doctor-dashboard/previous-history"
+              >
+                View Details
+              </Link>
+            </td>
+          </tr>
+        )}
+      </table>
+    </div>
+  );
 }
--- frontend/health-connect/src/hooks/Hospital/useHospitalProfileInfo.js
+++ frontend/health-connect/src/hooks/Hospital/useHospitalProfileInfo.js
@@ -2,50 +2,47 @@ import { useEffect, useState } from "react";
 import HospitalApi from "../../apis/HospitalApi";
 
 export const useHospitalProfileInfo = (user, hospitalId) => {
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        console.log("SINGLE APPOINTMENT COMPONENT LOADED...");
-        const fetchHospitalDetails = async () => {
-            try {
-                setLoading(true);
-                console.log(
-                    "SINGLE APPOINTMENT COMPONENT TRY CATCH...",
-                    user.token
-                );
-                const response = await HospitalApi.post(
-                    "/get-hospital",
-                    {
-                        hospitalId: hospitalId,
-                    },
-                    {
-                        headers: {
-                            "Content-Type": "application/json",
-                            Authorization: `Bearer ${user.token}`,
-                        },
-                    }
-                );
+  useEffect(() => {
+    console.log("SINGLE APPOINTMENT COMPONENT LOADED...");
+    const fetchHospitalDetails = async () => {
+      try {
+        setLoading(true);
+        console.log("SINGLE APPOINTMENT COMPONENT TRY CATCH...", user.token);
+        const response = await HospitalApi.post(
+          "/get-hospital",
+          {
+            hospitalId: hospitalId,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${user.token}`,
+            },
+          }
+        );
 
-                console.log("GET HOSPITAL HOOK", response.data);
-                setLoading(false);
-                setData(response.data); // Uncomment this line
-            } catch (err) {
-                console.log("--GET HOSPITAL HOOK ERROR--");
-                console.log(err);
-                setLoading(false);
-                setError(true);
-            }
-        };
-
-        fetchHospitalDetails();
-    }, []);
-
-    console.log("HELLO");
-    return {
-        data,
-        loading,
-        error,
+        console.log("GET HOSPITAL HOOK", response.data);
+        setLoading(false);
+        setData(response.data); // Uncomment this line
+      } catch (err) {
+        console.log("--GET HOSPITAL HOOK ERROR--");
+        console.log(err);
+        setLoading(false);
+        setError(true);
+      }
     };
+
+    fetchHospitalDetails();
+  }, []);
+
+  console.log("HELLO");
+  return {
+    data,
+    loading,
+    error,
+  };
 };
